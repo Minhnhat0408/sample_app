d6c011b9d6d5efd5d9a2ceed80bcb01d85c5d2f2
Minhnhat0408 <kurominhnhat@gmail.com>
2024-07-08 22:21:13 +0700

    Chapter 3,4,5
    init project

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  scope "(:locale)", locale: /en|vi/ do
+    get "home", to: "static_pages#home", as: :home
+    get "help", to: "static_pages#help", as: :help
+  end
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
   # Defines the root path route ("/")
