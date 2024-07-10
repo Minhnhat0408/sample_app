deadadaf86c858c3cdedbdaf0da018105c185449
Minhnhat0408 <kurominhnhat@gmail.com>
2024-07-10 17:30:50 +0700

    Chapter 7

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,9 +1,12 @@
 class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+  INPUT_VALID_ATTRIBUTES = %i(name email password password_confirmation).freeze
 
   before_save :downcase_email
-  validates :name, presence: true, length: {maximum: Settings.models.user.name_length}
-  validates :email, presence: true, length: {maximum: Settings.models.user.email_length},
+  validates :name, presence: true,
+    length: {maximum: Settings.models.user.name_length}
+  validates :email, presence: true,
+    length: {maximum: Settings.models.user.email_length},
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 
   has_secure_password
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,10 @@
 Rails.application.routes.draw do
   scope "(:locale)", locale: /en|vi/ do
+    root "static_pages#home"
     get "home", to: "static_pages#home", as: :home
     get "help", to: "static_pages#help", as: :help
+    get "/signup", to: "users#new"
+    post "/signup", to: "users#create"
+    resources :users, only: %i(new cqreate show)
   end
 end
