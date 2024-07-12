af9a540edef2f03405af6c39115e4872b1c0eb7b
Minhnhat0408 <kurominhnhat@gmail.com>
2024-07-12 12:59:03 +0700

    Chapter 9
    Chapter 7
    
    Chapter 8

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,7 +2,10 @@ class User < ApplicationRecord
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
   INPUT_VALID_ATTRIBUTES = %i(name email password password_confirmation).freeze
 
+  attr_accessor :remember_token
+
   before_save :downcase_email
+
   validates :name, presence: true,
     length: {maximum: Settings.models.user.name_length}
   validates :email, presence: true,
@@ -10,6 +13,19 @@ class User < ApplicationRecord
     format: {with: VALID_EMAIL_REGEX}, uniqueness: {case_sensitive: false}
 
   has_secure_password
+
+  def remember
+    self.remember_token = User.new_token
+    update_column :remember_digest, User.digest(remember_token)
+  end
+
+  def forget
+    update_column :remember_digest, nil
+  end
+
+  def authenticated? remember_token
+    BCrypt::Password.new(remember_digest).is_password? remember_token
+  end
 
   class << self
     def digest string
@@ -19,6 +35,10 @@ class User < ApplicationRecord
                BCrypt::Engine.cost
              end
       BCrypt::Password.create string, cost
+    end
+
+    def new_token
+      SecureRandom.urlsafe_base64
     end
   end
 
