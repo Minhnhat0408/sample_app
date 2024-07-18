6172bba6904abc7ce88c2eb2c1edcb128ac56241
Minhnhat0408 <kurominhnhat@gmail.com>
2024-07-18 15:35:21 +0700

    Chapter 14

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -54,6 +54,18 @@ class UsersController < ApplicationController
     redirect_to users_path
   end
 
+  def following
+    @title = t "following"
+    @pagy, @users = pagy @user.following, items: Settings.page_10
+    render :show_follow
+  end
+
+  def followers
+    @title = t "followers"
+    @pagy, @users = pagy @user.followers, items: Settings.page_10
+    render :show_follow
+  end
+
   private
 
   def admin_user
